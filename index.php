3bc1acd1eb1152983eb02d76b5ae5a4891436b4d
toni haryanto <toha.samba@gmail.com>
2013-05-19 23:23:24 +0700

    change query string to uri string

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
-
+	// parse uri
+	$uri = explode("/", $_SERVER['REQUEST_URI']);
+	
 	include "libs/raintpl/rain.tpl.class.php";
-
+	
+	raintpl::configure('base_url', 'http://localhost/layoutr/');
 	raintpl::configure("tpl_dir", "layouts/" );
 	raintpl::configure("cache_dir", "tmp/" );
 
 	$tpl = new RainTPL;
-
-	$layout = (isset($_GET['l'])) ? $_GET['l'] : 'homepage1';
-	$html = $tpl->draw($layout,true);
-    echo $html;
+	
+	$layout = (isset($uri[2])) ? $uri[2] : 'homepage1';
+	$tpl->draw($layout);
 	
 ?>
