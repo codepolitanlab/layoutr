d46f063d3f0c92df6ede37bbf2d388ed7de31b2d
toni haryanto <toha.samba@gmail.com>
2013-05-19 23:34:34 +0700

    move bootstrap files to themes/

--- index.php
+++ index.php
@@ -2,13 +2,15 @@
 	// parse uri
 	$uri = explode("/", $_SERVER['REQUEST_URI']);
 	
-	include "libs/raintpl/rain.tpl.class.php";
-	
+	include "raintpl/rain.tpl.class.php";
 	raintpl::configure('base_url', 'http://localhost/layoutr/');
 	raintpl::configure("tpl_dir", "layouts/" );
 	raintpl::configure("cache_dir", "tmp/" );
 
 	$tpl = new RainTPL;
+	
+	// set this to your own theme folder name
+	$tpl->assign('theme', 'bootstrap');
 	
 	$layout = (isset($uri[2])) ? $uri[2] : 'homepage1';
 	$tpl->draw($layout);
