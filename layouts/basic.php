d09fd6cc6034ed8a409f6135ec1732789790ee5b
toni haryanto <toha.samba@gmail.com>
2013-05-19 21:40:03 +0700

    initial commit

---
+++ layouts/basic.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include_once('partials/metadata.php'); ?>
+</head>
+<body>
+  <div class="container">
+    <header>
+      <?php include_once('partials/header1.php'); ?>      
+    </header>
+
+    <div class="top-content">
+      <?php include_once('partials/jumbotron1.php'); ?>
+      <hr>
+    </div>
+
+    <div class="content">
+      <div class="row-fluid">
+        <div class="span4">
+          <!-- Example row of columns -->
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div>
+        <div class="span4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div>
+        <div class="span4">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div>
+      </div>
+      <hr>
+    </div>
+
+
+    <footer>
+      <p>&copy; Company 2013</p>
+    </footer>
+
+  </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src=".../bootstrap/js/jquery.js"></script>
+    <script src=".../bootstrap/js/bootstrap-transition.js"></script>
+    <script src=".../bootstrap/js/bootstrap-alert.js"></script>
+    <script src=".../bootstrap/js/bootstrap-modal.js"></script>
+    <script src=".../bootstrap/js/bootstrap-dropdown.js"></script>
+    <script src=".../bootstrap/js/bootstrap-scrollspy.js"></script>
+    <script src=".../bootstrap/js/bootstrap-tab.js"></script>
+    <script src=".../bootstrap/js/bootstrap-tooltip.js"></script>
+    <script src=".../bootstrap/js/bootstrap-popover.js"></script>
+    <script src=".../bootstrap/js/bootstrap-button.js"></script>
+    <script src=".../bootstrap/js/bootstrap-collapse.js"></script>
+    <script src=".../bootstrap/js/bootstrap-carousel.js"></script>
+    <script src=".../bootstrap/js/bootstrap-typeahead.js"></script>
+
+  </body>
+  </html>
