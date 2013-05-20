049cc86867d821e51a79d91667103c764ca8463f
toni haryanto <toha.samba@gmail.com>
2013-05-20 13:10:40 +0700

    try to add new layout

--- index.php
+++ index.php
@@ -1,18 +1,42 @@
 <?php
-	// parse uri
-	$uri = explode("/", $_SERVER['REQUEST_URI']);
-	
+/* iclude rainTPL and configure it ================================== */
 	include "raintpl/rain.tpl.class.php";
 	raintpl::configure('base_url', 'http://localhost/layoutr/');
 	raintpl::configure("tpl_dir", "layouts/" );
 	raintpl::configure("cache_dir", "tmp/" );
+/* ================================================================== */
+	
+/* set some variables =============================================== */
+	// parse uri for layout
+	$uri = explode("/", $_SERVER['REQUEST_URI']);
+	$layout = (!empty($uri[2])) ? $uri[2] : 'homepage1';
+	
+	// folder theme name, set this to your own theme
+	$theme = 'bootstrap';
 
+	// this is custom style, make it the same name with its layout, otherwise it will use default style
+	$style = file_exists("themes/".$theme."/css/".$layout.'.css')? $layout : null;
+
+/* ================================================================== */
+
+/* Begin code ======================================================= */
 	$tpl = new RainTPL;
 	
-	// set this to your own theme folder name
-	$tpl->assign('theme', 'bootstrap');
-	
-	$layout = (isset($uri[2])) ? $uri[2] : 'homepage1';
-	$tpl->draw($layout);
-	
+	// assign some variables
+	$tpl->assign('theme', $theme); 
+	$tpl->assign('style', $style);
+
+	$header = isset($_GET['h'])? $_GET['h']: 'header1';
+	$tpl->assign('header', $header);
+
+	$content = isset($_GET['c'])? $_GET['c']: 'content1';
+	$tpl->assign('content', $content);	
+
+	$footer = isset($_GET['f'])? $_GET['f']: 'footer1';
+	$tpl->assign('footer', $footer);
+
+	// render
+	$tpl->draw($layout); 
+/* ================================================================== */	
+
 ?>
